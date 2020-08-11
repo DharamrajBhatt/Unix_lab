1b9cc34ef8fe5ce7a94997fc4f26d428e2095b4a
Dharamraj Bhatt <dharamrajbhatt97@gmail.com>
2020-08-11 23:49:11 +0530

    Add files via upload

---
+++ lab9/program1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<sys/types.h>
+int main()
+{      
+	for(int i=0;i<5;i++)
+	{
+
+		if(fork() == 0)
+		{
+			printf("[son] pid %d from [parent] pid %d\n",getpid(),getppid());
+			exit(0);
+		}
+	}
+	for(int i=0;i<5;i++)
+	pid=wait(&status);
+        
+
+}
+
