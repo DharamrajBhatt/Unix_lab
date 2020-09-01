5d40d7b90c8814fcfab6f93cbe1bc5f491c02ce3
Dharamraj Bhatt <dharamrajbhatt97@gmail.com>
2020-09-01 20:42:55 +0530

    Add files via upload

---
+++ lab#11/program.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+unsigned int data2;                                
+int data=23;
+int main()
+{
+//   int data1;
+   int data1=23;
+  // static int num;
+  static int num=12;
+   return 0;
+}
