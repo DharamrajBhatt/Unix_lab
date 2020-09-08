b403e24e0cf9b599b1862492de6e04749a886968
Dharamraj Bhatt <dharamrajbhatt97@gmail.com>
2020-09-08 22:53:11 +0530

    Update 3.c

--- lab#19/3.c
+++ lab#19/3.c
@@ -7,22 +7,23 @@ void main()
   int fp[2],a;
   char ch[12];
 //char ch1[12]="";
-  pipe(fp);
-  a=fork();
-  if(a==0)
+  pipe(fp); //creating pipe
+  a=fork(); //system call(process cration)
+  if(a==0)  //child process
   {
-    printf("child id=%d\n",getpid());
-    fp[1]=open("d.txt",O_WRONLY);//file open in write only mode
-    write(fp[1],"Hello World",12);
-    close(fp[1]);
+    printf("child id=%d\n",getpid());//printing id
+    fp[1]=open("d.txt",O_WRONLY); //opening file d.txt  open in write only mode
+    write(fp[1],"Hello World",12);// writing in the file(through pipe) 
+    close(fp[1]);//closing fp[1]
   }
-  else
+  else  //parent process
   {
 //wait(NULL);
-    printf("parent id=%d\n",getpid());
-    fp[0]=open("d.txt",O_RDONLY);//file open in read only mode
-    read(fp[0],ch,12);
-    printf(" =%s",ch);
-    close(fp[0]);
+    printf("parent id=%d\n",getpid());//printing id
+    fp[0]=open("d.txt",O_RDONLY);// opening the file d.txt  in read only mode
+    read(fp[0],ch,12); //reading from the file
+    printf(" =%s",ch); //printing the content of the file
+    close(fp[0]);//closing fp[0]
   }
 }
+
