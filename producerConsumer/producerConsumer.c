51329cdfd6ee8f3251c8ba08d5bcb2704cfea396
Dharamraj Bhatt <dharamrajbhatt97@gmail.com>
2020-09-14 21:17:51 +0530

    Update producerConsumer.c

--- producerConsumer/producerConsumer.c
+++ producerConsumer/producerConsumer.c
@@ -5,11 +5,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/*
-This program provides a possible solution for producer-consumer problem using mutex and semaphore.
-I have used 5 producers and 5 consumers to demonstrate the solution. You can always play with these values.
-*/
-
 #define MaxItems 3 // Maximum items a producer can produce or a consumer can consume
 #define BufferSize 3 // Size of the buffer
 
